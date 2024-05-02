2d31fc27e2b70d31cfff7303edbebbca171f72e7
TiagoBatixta <tiagobatista-slv@outlook.com>
2024-05-02 18:05:36 -0300

    index_update

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return render(request, "galeria/index.html")
+    
+    dados = {
+    1: {"nome": "Nebulosa", 
+        "legenda": "webbtelescope.org/ NASA / James Webb Space"},
+    2: {"nome": "Galaxia NGC",
+        "legenda": "nasa.org/ NASA / Hubble & NASA / J. Lee e A. Filippenko"}
+    }    
+    return render(request, "galeria/index.html", {"cards": dados})
 
 def imagem(request):
     return render(request, "galeria/imagem.html")
